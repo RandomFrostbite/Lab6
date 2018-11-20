5f0e14deb0211f5de7dc819663f490aeb56712ce
RandomFrostbite <il.zainutdinov@gmail.com>
2018-11-20 12:45:29 +0100

    Steps 1-10 Done

--- app/src/main/java/com/nvwa/lab6/MainActivity.java
+++ app/src/main/java/com/nvwa/lab6/MainActivity.java
@@ -1,13 +1,26 @@
 package com.nvwa.lab6;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    static public SensorManager mSensorManager;
+    static List<Sensor> SensorList;
+    static final public String SENSOR_TYPE = "sensorType";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        SensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
         setContentView(R.layout.activity_main);
     }
 }
