1f12a0a17745e344770be67458ff0e3ca1b4f4a5
RandomFrostbite <il.zainutdinov@gmail.com>
2018-11-20 19:55:15 +0100

    Lab6 Done

--- app/src/main/java/com/nvwa/lab6/SensorActivity.java
+++ app/src/main/java/com/nvwa/lab6/SensorActivity.java
@@ -1,9 +1,15 @@
 package com.nvwa.lab6;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -14,6 +20,7 @@ import org.w3c.dom.Text;
 public class SensorActivity extends AppCompatActivity implements SensorEventListener {
 
     Sensor mSensor;
+    boolean isFlashOn = false, hasFlash = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,18 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         }
         TextView valueTxtV = (TextView)findViewById(R.id.sensVals);
         valueTxtV.setText(valueText);
+
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT || event.sensor.getType() == Sensor.TYPE_PROXIMITY ) {
+            float sensVal = event.values[0];
+
+            if (hasFlash) {
+                if ( (event.sensor.getType() == Sensor.TYPE_LIGHT && sensVal<100) ||
+                        (event.sensor.getType() == Sensor.TYPE_PROXIMITY && sensVal == 0) ) {
+                    turnOnFlash();
+                } else
+                    turnOffFlash();
+            }
+        }
     }
 
     @Override
@@ -52,11 +71,66 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         MainActivity.mSensorManager.registerListener(this, mSensor, 500000 );
         TextView labelTxtV = (TextView)findViewById(R.id.sensLabel);
         labelTxtV.setText(mSensor.getName());
+
+        if (mSensor.getType() == Sensor.TYPE_LIGHT || mSensor.getType() == Sensor.TYPE_PROXIMITY ) {
+            hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+            if (!hasFlash){
+                Toast.makeText(getApplicationContext(), "Your device doesn't have a flashlight", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         MainActivity.mSensorManager.unregisterListener(this, mSensor);
+
+        if (isFlashOn)
+            turnOffFlash();
+    }
+
+    private void turnOnFlash() {
+        if (!isFlashOn) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                Camera camera = Camera.open();
+                Camera.Parameters params = camera.getParameters();
+                params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                camera.setParameters(params);
+                camera.startPreview();
+            } else {
+                CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                String cameraId = null;
+                try {
+                    cameraId = camManager.getCameraIdList()[0];
+                    camManager.setTorchMode(cameraId, true );
+                } catch ( CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            isFlashOn = true;
+        }
+    }
+
+    private void turnOffFlash() {
+        if (!isFlashOn) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                Camera camera = Camera.open();
+                Camera.Parameters params = camera.getParameters();
+                params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                camera.setParameters(params);
+                camera.startPreview();
+            } else {
+                CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                String cameraId = null;
+                try {
+                    cameraId = camManager.getCameraIdList()[0];
+                    camManager.setTorchMode(cameraId, false);
+                } catch ( CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            isFlashOn = false;
+        }
+
     }
 }
